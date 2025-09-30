9d24bdd26bd5cb297c4431c582e9367f3a2ff991
PainIam <tensie334@gmail.com>
2025-09-30 10:52:40 +0200

    ADD

---
+++ multdiv.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+using namespace std;
+
+int main() {
+    ll t,n;
+    vector<int>v;
+    cin >> t;
+
+    while(t--) {
+        cin >> n;
+        int a,b,count;
+        a=b=count=0;
+
+        //count a's
+        ll tmp = n;
+        while(tmp%2==0) {
+            a++;
+            tmp/=2;
+        }
+        //count b's
+        while(tmp%3==0) {
+            b++;
+            tmp/=3;
+        }
+
+        if (tmp!=1 || a>b) {
+            cout << "-1\n";
+        } else {
+            int x = (b-a);
+            count = x+b;
+            v.pb(count);
+        }
+        
+    }
+    
+    for(auto&x:v)
+        cout << x << "\n";
+}
